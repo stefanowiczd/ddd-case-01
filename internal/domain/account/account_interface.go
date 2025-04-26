7f718f73b1885cb750f867e763da99641f86d795
Dominik Stefanowicz <dominik.stefanowicz@gmail.com>
2025-04-26 06:26:54 +0200

    account domain - updated package name for mock

--- internal/domain/account/account_interface.go
+++ internal/domain/account/account_interface.go
@@ -2,12 +2,19 @@ package account
 
 import "time"
 
-//go:generate mockgen -destination=./mock/account_mock.go -package=account -source=./account_interface.go
+//go:generate mockgen -destination=./mock/account_mock.go -package=mock -source=./account_interface.go
 
 // Event represents a domain event
 type Event interface {
+	// GetID returns the unique identifier of the event
 	GetID() string
+
+	// GetType returns the type of the event
 	GetType() string
+
+	// GetAggregateID returns the unique identifier of the aggregate that the event belongs to
 	GetAggregateID() string
+
+	// GetCreatedAt returns the date and time the event was created
 	GetCreatedAt() time.Time
 }
